2183e3efe3f4162477a8cfca7cc8d40ed82043eb
Gabriel Sousa <gabrielsousa.ti@gmail.com>
2025-08-26 13:35:25 -0300

    Adiciona interação ao adicionar dígitos

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
-const elementsHTMLGame = [];
+const boardElementsHTML = [];
 var selectedCell;
+var selectedCellRow;
+var selectedCellCol;
+var activeGridCellsHTML = [];
 var activeGridCells = [];
 var selectedNumbersCells = [];
 
 // TODO: criar função para gerar jogo válido
 const boardFull = [];
 const boardPlayable = [];
-const digits = [1,2,3,4,5,6,7,8,9];
+const positionsCandidate = [];
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const digitsElementsHTML = [];
 
 const subgridLenght = 3;
 
 const ATTRIBUTE_DATA_SUBGRID = "data-subgrid";
 const ATTRIBUTE_DATA_ROW = "data-row";
 const ATTRIBUTE_DATA_COL = "data-col";
+const ATTRIBUTE_DATA_DIGIT = "data-digit";
 const CLASS_SELECTED_CELL = "selected-cell";
 const CLASS_ACTIVE = "active";
 const CLASS_SUB_ACTIVE = "sub-active";
+const CLASS_INSERTED_SUCCESS = "inserted-success";
 
 
+// TODO: colocar para chamar estas funções somente quando for clicado em algum botão "Inicia Jogo"
 initGame();
+loadHTMLElements();
+loadDigitsHTMLElements();
+makeGridInteractive();
+makeDigitsInteractive();
 
 function initGame() {
 
@@ -32,7 +44,7 @@ function initGame() {
         Difícil: entre 28 a 31 números.
         Expert/Desafiador: pode ter menos de 28 números.
     */
-    removeNumbers(boardPlayable, 80);
+    removeNumbers(boardPlayable, 75);
 }
 
 function removeNumbers(board, numbersToKeep = 35) {
@@ -52,9 +64,10 @@ function removeNumbers(board, numbersToKeep = 35) {
         const backup = board[row][col];
         board[row][col] = 0;
 
-        // Aqui você deveria verificar se ainda há uma única solução.
-        // Para simplificar, vamos pular essa verificação por enquanto.
-        // Em produção, você usaria um solver para validar.
+        // TODO: utilizar um solver aqui para ver se tem apenas uma unica solução
+
+        // Adiciona posições removidas(que podem ser preenchidas) para fazer verificações durante o jogo
+        positionsCandidate.push([row, col]);
 
         removed++;
         if (removed >= totalToRemove) break;
@@ -114,33 +127,41 @@ function generateSudokuBoard() {
 }
 
 // recupera célula pelos atributos personalizados data-row(linha) e data-col (coluna)
-function loadCell(row, col) {
-    return document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+function getCellHtml(row, col) {
+    return document.querySelector(`[${ATTRIBUTE_DATA_ROW}="${row}"][${ATTRIBUTE_DATA_COL}="${col}"]`);
+}
+
+// recupera elemento html do digito
+function getDigitHtml(digit) {
+    return document.querySelector(`[data-digit="${digit}"]`);
 }
 
 function loadHTMLElements() {
     for (let row = 0; row < boardPlayable.length; row++) {
         var rowList = []
         for (let col = 0; col < boardPlayable.length; col++) {
-            let cell = loadCell(row, col);
+            let cell = getCellHtml(row, col);
             rowList.push(cell)
 
             // TODO: chegagem para ocultar números baseado no nível
             if (boardPlayable[row][col] != 0) cell.innerHTML = boardPlayable[row][col];
         }
-        elementsHTMLGame.push(rowList)
+        boardElementsHTML.push(rowList)
     }
 }
 
-// TODO: colocar para chamar estas funções somente quando for clicado em algum botão "Inicia Jogo"
-loadHTMLElements();
-makeGridInteractive();
-makeDigitsInteractive();
+function loadDigitsHTMLElements() {
+    for (let i = 0; i < digits.length; i++) {
+        digitsElementsHTML.push(getDigitHtml(digits[i]));
+    }
+    updateDigits()
+}
 
-// Adicionar interação nos dígitos
-function makeDigitsInteractive() {
-    for (let row = 0; row < digits.length; row++) {
-        console.log(digits);
+
+// TODO: atualiza digitos para ocultar caso estejam todos preenchidos e todos corretos
+function updateDigits() {
+    for (let i = 0; i < digitsElementsHTML.length; i++) {
+        // console.log(digitsElementsHTML[i]);
     }
 }
 
@@ -153,6 +174,13 @@ function makeGridInteractive() {
     }
 }
 
+// Adicionar interação nos dígitos
+function makeDigitsInteractive() {
+    for (let i = 0; i < digits.length; i++) {
+        addClickDigit(i);
+    }
+}
+
 /**
  * 
  * Clique na célula deve fazer:
@@ -161,12 +189,12 @@ function makeGridInteractive() {
  *  - realçar todo subgrid que a célula pertence 
  */
 function addClickCell(row, col) {
-    let cell = elementsHTMLGame[row][col];
+    let cell = boardElementsHTML[row][col];
 
     cell.onclick = function (event) {
         // limpa visualização de cores do grid
-        clearGridLayout();
-        updateSelectedCell(cell);
+        clearGridLayout(true);
+        updateSelectedCell(cell, row, col);
         updateSelectedNumbersListCells();
         updateActiveGridCells(row, col);
         handleAllCellColors(cell);
@@ -174,33 +202,75 @@ function addClickCell(row, col) {
     };
 }
 
-function updateSelectedCell(cell) {
+/**
+ * 
+ * Clique no dígito deve fazer:
+ *  - adicionar numero clicado à célula que está selecionada
+ */
+function addClickDigit(i) {
+    let digit = digitsElementsHTML[i];
+    digit.onclick = function (event) {
+        // limpa visualização de cores do grid
+        clearGridLayout(false);
+
+        // só posso adicionar o número em lugares que o valor está na lista de valores removidos e se tiver célula selecionada
+        if (selectedCell && positionsCandidate.some(([row, col]) => row == selectedCellRow && col == selectedCellCol)) {
+            selectedCell.innerHTML = digit.innerHTML
+            boardPlayable[selectedCellRow][selectedCellCol] = parseInt(digit.innerHTML);
+
+            // chama método para pintar celulas iguais ao número escolhido 
+            handleAllCellColors(selectedCell);
+            updateColorByBehavior(boardElementsHTML[selectedCellRow][selectedCellCol]);
+
+            // TODO: Esconder dígitos que já foram todos preenchidos
+            updateDigits();
+            verifica();
+        }
+    };
+}
+
+function updateColorByBehavior(element) {
+    // Adiciona cor azul se o número inserido for correto
+    if (element) {
+        addClass(element, CLASS_INSERTED_SUCCESS);
+    }
+}
+
+function updateSelectedCell(cell, row, col) {
     selectedCell = cell;
+    selectedCellRow = row;
+    selectedCellCol = col;
     addClass(selectedCell, CLASS_SELECTED_CELL);
 }
 
 function updateActiveGridCells(row, col) {
-    // atualiza lista de celulas do grid selecionado
-    activeGridCells = document.querySelector(`[${ATTRIBUTE_DATA_SUBGRID}="${checkCellSubgrid(row, col)}"]`).children;
+    // Atualiza lista de celulas do grid selecionado
+    activeGridCellsHTML = document.querySelector(`[${ATTRIBUTE_DATA_SUBGRID}="${checkCellSubgrid(row, col)}"]`).children;
+
+    // Atualiza lista de subgrids Ex: ([0,0], ..., [0,2])
+    activeGridCells.length = 0;
+    for (const cellHtml of activeGridCellsHTML) {
+        activeGridCells.push(getRowColAttributeFromElementHtml(cellHtml));
+    }
 }
 
 function updateSelectedNumbersListCells() {
     // atualizar lista com a referencia de todas as celulas de mesmo número
     selectedNumbersCells.length = 0;
-    for (let i = 0; i < elementsHTMLGame.length; i++) {
-        for (let j = 0; j < elementsHTMLGame.length; j++) {
-            if (selectedCell.innerHTML === elementsHTMLGame[i][j].innerHTML) {
-                selectedNumbersCells.push(elementsHTMLGame[i][j]);
+    for (let i = 0; i < boardElementsHTML.length; i++) {
+        for (let j = 0; j < boardElementsHTML.length; j++) {
+            if (selectedCell.innerHTML === boardElementsHTML[i][j].innerHTML) {
+                selectedNumbersCells.push(boardElementsHTML[i][j]);
             }
         }
     }
 }
 
-// pinta números que estão
+// pinta números que estão no mesmo subgrid, linha e coluna
 function handleAllCellColors(cell) {
     for (let row = 0; row < boardFull.length; row++) {
         for (let col = 0; col < boardFull.length; col++) {
-            const currentCell = elementsHTMLGame[row][col];
+            const currentCell = boardElementsHTML[row][col];
             const selectedCellRow = getCustomAttribute(cell, ATTRIBUTE_DATA_ROW);
             const selectedCellCollumn = getCustomAttribute(cell, ATTRIBUTE_DATA_COL);
 
@@ -228,13 +298,17 @@ function getCustomAttribute(element, attribute) {
     return element.getAttribute(attribute)
 }
 
+function getRowColAttributeFromElementHtml(element) {
+    return [parseInt(element.getAttribute(ATTRIBUTE_DATA_ROW)), parseInt(element.getAttribute(ATTRIBUTE_DATA_COL))];
+}
+
 function checkCellSubgrid(row, col) {
-    return getCustomAttribute(elementsHTMLGame[row][col].parentElement, ATTRIBUTE_DATA_SUBGRID)
+    return getCustomAttribute(boardElementsHTML[row][col].parentElement, ATTRIBUTE_DATA_SUBGRID)
 }
 
 function handleActiveCellsSubgrid(row, col) {
-    for (let i = 0; i < activeGridCells.length; i++) {
-        let cellElementHTML = elementsHTMLGame[getCustomAttribute(activeGridCells[i], ATTRIBUTE_DATA_ROW)][getCustomAttribute(activeGridCells[i], ATTRIBUTE_DATA_COL)]
+    for (let i = 0; i < activeGridCellsHTML.length; i++) {
+        let cellElementHTML = boardElementsHTML[getCustomAttribute(activeGridCellsHTML[i], ATTRIBUTE_DATA_ROW)][getCustomAttribute(activeGridCellsHTML[i], ATTRIBUTE_DATA_COL)]
 
         if (cellElementHTML != selectedCell) {
             addClass(cellElementHTML, CLASS_SUB_ACTIVE)
@@ -242,21 +316,21 @@ function handleActiveCellsSubgrid(row, col) {
             addClass(cellElementHTML, CLASS_ACTIVE)
         }
     }
-
 }
 
-function clearGridLayout() {
-    for (let row = 0; row < elementsHTMLGame.length; row++) {
-        for (let col = 0; col < elementsHTMLGame.length; col++) {
-            removeClass(elementsHTMLGame[row][col], CLASS_SELECTED_CELL);
-            removeClass(elementsHTMLGame[row][col], CLASS_ACTIVE)
-            removeClass(elementsHTMLGame[row][col], CLASS_SUB_ACTIVE)
+function clearGridLayout(clearSelected) {
+    for (let row = 0; row < boardElementsHTML.length; row++) {
+        for (let col = 0; col < boardElementsHTML.length; col++) {
+            if (clearSelected) removeClass(boardElementsHTML[row][col], CLASS_SELECTED_CELL);
+            removeClass(boardElementsHTML[row][col], CLASS_ACTIVE)
+            removeClass(boardElementsHTML[row][col], CLASS_SUB_ACTIVE)
         }
     }
 }
 
 function verifica() {
     if (isAllRowsValid(boardPlayable) && isAllCollumnsValid(boardPlayable) && isAllSubgridsValid()) {
+        alert("Parabéns!! Jogo concluído com sucesso!")
         console.log("jogo válido!");
     } else {
         console.log("jogo inválido!");
@@ -304,15 +378,15 @@ function isSubgridValid(row, col) {
     const numbers = [];
     for (let i = row; i < row + 3; i++) {
         for (let j = col; j < col + 3; j++) {
-            numbers.push(boardFull[i][j]);
+            if (boardPlayable[i][j] > 0 && boardPlayable[i][j] <= 9) {
+                numbers.push(boardPlayable[i][j]);
+            }
         }
     }
 
-    return isNonRepeatedArray(numbers) && numbers.length != 9;
+    return isNonRepeatedArray(numbers) && numbers.length == 9;
 }
 
 function isNonRepeatedArray(array) {
     return new Set(array).size === array.length;
 }
-
-verifica()
